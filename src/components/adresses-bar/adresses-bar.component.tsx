9bf58a48dd881cdf8cd23c51b8144d9c0392f6d9
eduardo.correa <eduardo.correa03@edu.pucrs.br>
2024-05-13 23:13:49 -0300

    feat: a component was created for addresses and corrections in the style

--- src/components/adresses-bar/adresses-bar.component.tsx
+++ src/components/adresses-bar/adresses-bar.component.tsx
@@ -1,20 +1,29 @@
 import styles from "./adresses-bar.module.scss";
-import Image from "next/image";
-import locationIcon from "@public/location-icon.svg";
+import { Adress } from "../adress/adress.component";
 
 export function AdressesBar() {
   return (
     <div className={styles.container}>
-      <div className={styles.adress}>
-        <div className={styles.adressTitleLine}>
-          <Image src={locationIcon} alt={"Ícone de localização"} />
-          <span className={styles.adressTitle}>Petrópolis</span>
-        </div>
-        <span className={styles.adressDescription}>
-          Av. Cel. Lucas de Oliveira, 505 - sala 302 - Petrópolis, Porto Alegre
-          - RS, 90440-011
-        </span>
-      </div>
+      <Adress
+        title={"Petrópolis"}
+        description={
+          "Av. Cel. Lucas de Oliveira, 505 - sala 302 - Petrópolis, Porto Alegre - RS, 90440-011"
+        }
+      />
+      <Adress
+        title={"IGO Petrópolis"}
+        description={
+          "Av. Sen. Tarso Dutra, 10 - 3º andar - Petrópolis, Porto Alegre - RS, 90690-140"
+        }
+      />
+      <Adress
+        title={"IGO Canoas"}
+        description={"R. Liberdade, 33 - Mal. Rondon, Canoas - RS, 92020-030"}
+      />
+      <Adress
+        title={"Atendimento"}
+        description={"De segunda à sexta das 12:00 às 18h00. (51)999999999"}
+      />
     </div>
   );
 }
