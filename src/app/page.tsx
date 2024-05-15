caf2eae25e5dbc6a265abaf5ab4edd8b0a09c1eb
eduardo.correa <eduardo.correa03@edu.pucrs.br>
2024-05-15 10:51:02 -0300

    chore: minor fixes and mouse icon added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,11 @@
-import { ScheduleAppointment } from "@/components/schedule-appointment/schedule-appointment.component";
-import styles from "./page.module.scss";
-import { FrequentlyAskedQuestions } from "@/components/faq/faq.component";
 import { AdressesBar } from "@/components/adresses-bar/adresses-bar.component";
+import { InitialSection } from "@/components/initial-section/initial-section.component";
 
 export default function Home() {
   return (
     <>
       <AdressesBar />
-      <div className={styles.initialSectionContainer}>
-        <div className={styles.initialSectionContent}>
-          <span className={styles.doctorName}>Dr. Jefferson André Bauer</span>
-          <span className={styles.doctorBio}>
-            Otorrinolaringologista com mais de 5 anos de experiência
-            especializado em Otologia Cirúrgica, Otologia Clínica,
-            Otoneurologia.
-          </span>
-          <div className={styles.buttons}>
-            <ScheduleAppointment />
-            <FrequentlyAskedQuestions />
-          </div>
-        </div>
-      </div>
+      <InitialSection />
     </>
   );
 }
