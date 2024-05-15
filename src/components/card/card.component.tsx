9f34e65b230bb282fae35cc4fc306039d86bb75b
eduardo.correa <eduardo.correa03@edu.pucrs.br>
2024-05-15 16:05:38 -0300

    feat: create pathology card component

---
+++ src/components/card/card.component.tsx
@@ -0,0 +1,30 @@
+import styles from "./card.module.scss";
+import Image from "next/image";
+import bookIcon from "@public/book-open-01.svg";
+
+interface CardProps {
+  title: string;
+  text: string;
+  imagePath: string;
+}
+
+export function Card({ title, text, imagePath }: CardProps) {
+  return (
+    <div className={styles.container}>
+      <div
+        className={styles.imageContainer}
+        style={{ backgroundImage: `url(${imagePath})` }}
+      ></div>
+      <div className={styles.title}>{title}</div>
+      <div className={styles.text}>{text}</div>
+      <a className={styles.buttonContainer} href="#">
+        <p className={styles.buttonText}>Ver Artigo</p>
+        <Image
+          className={styles.buttonIcon}
+          src={bookIcon}
+          alt={"Ícone de comentário"}
+        />
+      </a>
+    </div>
+  );
+}
--- src/components/pathology-section/pathology-section.component.tsx
+++ src/components/pathology-section/pathology-section.component.tsx
@@ -1,9 +1,52 @@
+import { Card } from "../card/card.component";
 import styles from "./pathology-section.module.scss";
 
 export function PathologySection() {
   return (
     <div className={styles.container}>
       <span className={styles.title}>Patologias</span>
+      <div className={styles.cardsWrapper}>
+        <div className={styles.cardsContainer}>
+          <Card
+            title={"Manobras de reposicionamento para VPPB"}
+            text={
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, placeat?"
+            }
+            imagePath={"/IMG_2646.jpg"}
+          />
+          <Card
+            title={"Manobras de reposicionamento para VPPB"}
+            text={
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, placeat?"
+            }
+            imagePath={""}
+          />
+          <Card
+            title={"Manobras de reposicionamento para VPPB"}
+            text={
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, placeat?"
+            }
+            imagePath={""}
+          />
+          <Card
+            title={"Manobras de reposicionamento para VPPB"}
+            text={
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, placeat?"
+            }
+            imagePath={""}
+          />
+          <Card
+            title={"Manobras de reposicionamento para VPPB"}
+            text={
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, placeat?"
+            }
+            imagePath={""}
+          />
+        </div>
+      </div>
+      <a href="#">
+        <p className={styles.seeAll}>Veja todas -&gt;</p>
+      </a>
     </div>
   );
 }
