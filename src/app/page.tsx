b97444eab70abd714507465a2955fbe38b626ee7
eduardo.correa <eduardo.correa03@edu.pucrs.br>
2024-05-15 12:56:22 -0300

    feat: create presentation section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { AdressesBar } from "@/components/adresses-bar/adresses-bar.component";
+import { PresentationSection } from "@/components/presentation-section/presentation-section.component";
 
 export default function Home() {
   return (
     <>
       <AdressesBar />
+      <PresentationSection />
     </>
   );
 }
---
+++ src/components/presentation-section/presentation-section.component.tsx
@@ -0,0 +1,38 @@
+import styles from "./presentation-section.module.scss";
+
+export function PresentationSection() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageContainer}></div>
+      <div className={styles.contentContainer}>
+        <div className={styles.title}>Dr. Jefferson André Bauer</div>
+        <div className={styles.divider}></div>
+        <div className={styles.text}>
+          Otorrinolaringologista com mais de 5 anos de experiência especializado
+          em Otologia Cirúrgica, Otologia Clínica, Otoneurologia. O meu número
+          de registro profissional: 35975.
+        </div>
+        <div className={styles.divider}></div>
+        <div className={styles.title}>Formação</div>
+        <ul className={styles.text}>
+          <li>
+            - Mestrado em Cirurgia, Universidade Federal do Rio Grande do Sul,
+            2016
+          </li>
+          <li>
+            - Fellowship Otologia Clínica e Cirúrgica, Hospital Mãe de Deus,
+            2015
+          </li>
+          <li>
+            - Graduação em Medicina, Universidade Federal do Rio Grande do Sul,
+            2011
+          </li>
+          <li>
+            - Residência Médica em Otorrinolaringologia, Hospital de Clínicas de
+            Porto Alegre, 2014
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
