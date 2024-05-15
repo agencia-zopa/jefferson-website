2ca0da186338c7fa75a5b162017d67525cf07ea6
eduardo.correa <eduardo.correa03@edu.pucrs.br>
2024-05-15 13:18:15 -0300

    branch init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { AdressesBar } from "@/components/adresses-bar/adresses-bar.component";
+import { PathologySection } from "@/components/pathology-section/pathology-section.component";
 
 export default function Home() {
   return (
     <>
       <AdressesBar />
+      <PathologySection />
     </>
   );
 }
---
+++ src/components/pathology-section/pathology-section.component.tsx
@@ -0,0 +1,9 @@
+import styles from "./pathology-section.module.scss";
+
+export function PathologySection() {
+  return (
+    <div className={styles.container}>
+      <span className={styles.title}>Patologias</span>
+    </div>
+  );
+}
