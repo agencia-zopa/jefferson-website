64f282bc376b0b5f3603ff7d537d9ab3cb9c2640
eduardo.correa <eduardo.correa03@edu.pucrs.br>
2024-05-16 10:26:05 -0300

    feat: create pathologies page

---
+++ src/app/patologias/page.tsx
@@ -0,0 +1,70 @@
+import { PathologyCard } from "@/components/pathology-card/pathology-card.component";
+import styles from "./page.module.scss";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.sectionTitle}>
+        <span>Patologias</span>
+      </div>
+      <div className={styles.cardsContainer}>
+        <PathologyCard
+          title={"Aplicação de corticoide intratimpânico"}
+          text={
+            "Lorem ipsum dolor sit amet consectetur. Viverra nec viverra tortor risus in orci. Egestas pulvinar faucibus potenti nunc non tortor velit."
+          }
+          imagePath={"/IMG_2646.jpg"}
+        />
+        <PathologyCard
+          title={"Manobras de reposicionamento para VPPB"}
+          text={
+            "Lorem ipsum dolor sit amet consectetur. Viverra nec viverra tortor risus in orci. Egestas pulvinar faucibus potenti nunc non tortor velit."
+          }
+          imagePath={""}
+        />
+        <PathologyCard
+          title={"Tubo de ventilação"}
+          text={
+            "Lorem ipsum dolor sit amet consectetur. Viverra nec viverra tortor risus in orci. Egestas pulvinar faucibus potenti nunc non tortor velit."
+          }
+          imagePath={""}
+        />
+        <PathologyCard
+          title={"Timpanoplastia"}
+          text={
+            "Lorem ipsum dolor sit amet consectetur. Viverra nec viverra tortor risus in orci. Egestas pulvinar faucibus potenti nunc non tortor velit."
+          }
+          imagePath={""}
+        />
+        <PathologyCard
+          title={"Estapedectomia"}
+          text={
+            "Lorem ipsum dolor sit amet consectetur. Viverra nec viverra tortor risus in orci. Egestas pulvinar faucibus potenti nunc non tortor velit."
+          }
+          imagePath={"/IMG_2646.jpg"}
+        />
+        <PathologyCard
+          title={"Implante Coclear"}
+          text={
+            "Lorem ipsum dolor sit amet consectetur. Viverra nec viverra tortor risus in orci. Egestas pulvinar faucibus potenti nunc non tortor velit."
+          }
+          imagePath={""}
+        />
+        <PathologyCard
+          title={"Próteses ancoradas no osso (BAHA, Ponto, etc)"}
+          text={
+            "Lorem ipsum dolor sit amet consectetur. Viverra nec viverra tortor risus in orci. Egestas pulvinar faucibus potenti nunc non tortor velit."
+          }
+          imagePath={""}
+        />
+        <PathologyCard
+          title={"Próteses implantáveis"}
+          text={
+            "Lorem ipsum dolor sit amet consectetur. Viverra nec viverra tortor risus in orci. Egestas pulvinar faucibus potenti nunc non tortor velit."
+          }
+          imagePath={""}
+        />
+      </div>
+    </div>
+  );
+}
