42d7a65df24b96e7f94c89752db23bdab541f8ce
Fernando Picoral <fernandopicoral@gmail.com>
2024-05-24 22:09:59 -0700

    chore: added prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import "@/globals.scss";
 import React from "react";
-import { Navbar } from "@/components/navbar/navbar.component";
-import {Footer} from "@/components/footer/footer.component";
 
 export const metadata: Metadata = {
   title: "Teste",
@@ -15,11 +13,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body>
-        <Navbar />
         {children}
-        <Footer />
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,44 +11,52 @@ import { ProceduresSection } from "@/components/procedures-section/procedures-se
 import { ConveniosSection } from "@/components/convenios-section/convenios-section.component";
 import { ReviewsSection } from "@/components/reviews-section/reviews-section.component";
 import {FaqSection} from "@/components/faq-section/faq-section.component";
+import {Navbar} from '@/components/navbar/navbar.component';
+import React from 'react';
+import {Footer} from '@/components/footer/footer.component';
 
 export default function Home() {
   return (
     <>
-      <AdressesBar />
-      <InitialSection />
-      <PresentationSection />
-      <SpecialtySection
-        imageSrc={otologiaImage}
-        imageAlt="Dr. Jefferson segurando a cabeça de uma paciente"
-        title="Otologia"
-        articleHref="#"
-      >
-        A Otologia é uma especialidade médica focada no estudo e tratamento dos
-        distúrbios do ouvido e suas estruturas relacionadas. Isso inclui não
-        apenas o ouvido externo e médio, mas também o ouvido interno e as
-        conexões neurais que transmitem informações auditivas ao cérebro.
-      </SpecialtySection>
-      <SpecialtySection
-        imageSrc={otoneuroImage}
-        imageAlt="Dr. Jefferson examinando uma paciente"
-        title="Otoneurologia"
-        articleHref="#"
-        flipped
-      >
-        A Otoneurologia é uma subespecialidade médica que se concentra no estudo
-        e tratamento dos distúrbios relacionados ao ouvido interno e ao sistema
-        nervoso vestibular, responsável pelo equilíbrio e pela orientação
-        espacial. Ela combina os campos da otologia (estudo do ouvido) e da
-        neurologia (estudo do sistema nervoso) para abordar condições que afetam
-        tanto a audição quanto o equilíbrio.
-      </SpecialtySection>
-      <ProceduresSection />
-      <PathologySection />
-      <ReviewsSection />
-      <ScheduleAppointmentSection />
-      <ConveniosSection />
-      <FaqSection />
+        <Navbar />
+        <main>
+            <AdressesBar/>
+            <InitialSection/>
+            <PresentationSection/>
+            <SpecialtySection
+                imageSrc={otologiaImage}
+                imageAlt="Dr. Jefferson segurando a cabeça de uma paciente"
+                title="Otologia"
+                articleHref="#"
+            >
+                A Otologia é uma especialidade médica focada no estudo e tratamento dos
+                distúrbios do ouvido e suas estruturas relacionadas. Isso inclui não
+                apenas o ouvido externo e médio, mas também o ouvido interno e as
+                conexões neurais que transmitem informações auditivas ao cérebro.
+            </SpecialtySection>
+            <SpecialtySection
+                imageSrc={otoneuroImage}
+                imageAlt="Dr. Jefferson examinando uma paciente"
+                title="Otoneurologia"
+                articleHref="#"
+                flipped
+            >
+                A Otoneurologia é uma subespecialidade médica que se concentra no estudo
+                e tratamento dos distúrbios relacionados ao ouvido interno e ao sistema
+                nervoso vestibular, responsável pelo equilíbrio e pela orientação
+                espacial. Ela combina os campos da otologia (estudo do ouvido) e da
+                neurologia (estudo do sistema nervoso) para abordar condições que afetam
+                tanto a audição quanto o equilíbrio.
+            </SpecialtySection>
+            <ProceduresSection/>
+            <PathologySection/>
+            <ReviewsSection/>
+            <ScheduleAppointmentSection/>
+            <ConveniosSection/>
+            <FaqSection/>
+        </main>
+        <Footer />
     </>
-  );
+)
+    ;
 }
