cb65e72f08b59112f73ace4fbbd6f57d2b36d0e3
Lucas Antunes <lucasba8@gmail.com>
2024-05-27 10:51:44 -0300

    feat: lint fix

--- src/components/page-title/page-title.component.tsx
+++ src/components/page-title/page-title.component.tsx
@@ -1,12 +1,16 @@
+import arrow from '@public/arrow-left-02-round.svg';
+import Image from 'next/image';
 import { PropsWithChildren } from 'react';
 
 import styles from './page-title.module.scss';
-import Image from "next/image";
-import arrow from '@public/arrow-left-02-round.svg'
 
 export function PageTitle({ children }: PropsWithChildren) {
-  return <h1 className={styles.pageTitle}>
-    <a href="/"><Image src={arrow} alt="Icone de seta de voltar" /></a>
-    {children}
-  </h1>;
+  return (
+    <h1 className={styles.pageTitle}>
+      <a href={'/'}>
+        <Image src={arrow} alt={'Icone de seta de voltar'} />
+      </a>
+      {children}
+    </h1>
+  );
 }
