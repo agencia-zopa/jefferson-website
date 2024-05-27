747cfe3ba7503ea3e63b78ea43ffad4f8539750d
Lucas Antunes <lucasba8@gmail.com>
2024-05-27 10:51:32 -0300

    feat: improvemeeeents

--- src/components/page-title/page-title.component.tsx
+++ src/components/page-title/page-title.component.tsx
@@ -1,7 +1,12 @@
 import { PropsWithChildren } from 'react';
 
 import styles from './page-title.module.scss';
+import Image from "next/image";
+import arrow from '@public/arrow-left-02-round.svg'
 
 export function PageTitle({ children }: PropsWithChildren) {
-  return <h1 className={styles.pageTitle}>{children}</h1>;
+  return <h1 className={styles.pageTitle}>
+    <a href="/"><Image src={arrow} alt="Icone de seta de voltar" /></a>
+    {children}
+  </h1>;
 }
