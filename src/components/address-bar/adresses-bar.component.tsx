dcd131ea8ab06a38e0c1a857c7330b2ee435990d
Lucas Antunes <lucasba8@gmail.com>
2024-06-18 13:48:58 -0300

    refact: lint fix

--- src/components/address-bar/adresses-bar.component.tsx
+++ src/components/address-bar/adresses-bar.component.tsx
@@ -1,8 +1,8 @@
 import locationIcon from '@public/location-icon.svg';
 import Image from 'next/image';
+import { ReactNode } from 'react';
 
 import styles from './adresses-bar.module.scss';
-import {ReactNode} from "react";
 
 interface Address {
   title: string;
@@ -13,21 +13,41 @@ export function AddressBar() {
   const addresses: Address[] = [
     {
       title: 'Consultório Mont’Serrat',
-      description:
-        <span>Av. Cel. Lucas De Oliveira, 505 - Sala 302 - Petrópolis, Porto Alegre - RS, <span className={styles.noWrap}>90440-011</span></span>
+      description: (
+        <span>
+          Av. Cel. Lucas De Oliveira, 505 - Sala 302 - Petrópolis, Porto Alegre
+          - RS, <span className={styles.noWrap}>90440-011</span>
+        </span>
+      )
     },
     {
       title: 'IGO Petrópolis',
-      description:
-        <span>Av. Sen. Tarso Dutra, 10 - 3º Andar - Petrópolis, Porto Alegre - RS, <span className={styles.noWrap}>90690-140</span></span>
+      description: (
+        <span>
+          Av. Sen. Tarso Dutra, 10 - 3º Andar - Petrópolis, Porto Alegre - RS,{' '}
+          <span className={styles.noWrap}>90690-140</span>
+        </span>
+      )
     },
     {
       title: 'IGO Canoas',
-      description: <span>R. Liberdade, 33 - Mal. Rondon, Canoas - RS, <span className={styles.noWrap}>92020-030</span></span>
+      description: (
+        <span>
+          R. Liberdade, 33 - Mal. Rondon, Canoas - RS,{' '}
+          <span className={styles.noWrap}>92020-030</span>
+        </span>
+      )
     },
     {
       title: 'Atendimento',
-      description: <span>De Segunda À Sexta Das 08:00 às 19h00. <span className={`${styles.noWrap} ${styles.bold}`}>(51) 99152-6125</span></span>
+      description: (
+        <span>
+          De Segunda À Sexta Das 08:00 às 19h00.{' '}
+          <span className={`${styles.noWrap} ${styles.bold}`}>
+            (51) 99152-6125
+          </span>
+        </span>
+      )
     }
   ];
 
--- src/components/mobile-address-bar/mobile-adresses-bar.component.tsx
+++ src/components/mobile-address-bar/mobile-adresses-bar.component.tsx
@@ -2,7 +2,7 @@ import ArrowRight from '@public/arrow-right.svg';
 import closeIcon from '@public/close.svg';
 import locationIcon from '@public/location-icon.svg';
 import Image from 'next/image';
-import {ReactNode, useState} from 'react';
+import { ReactNode, useState } from 'react';
 
 import styles from './mobile-adresses-bar.module.scss';
 
@@ -15,21 +15,41 @@ export function MobileAddressBar() {
   const addresses: Address[] = [
     {
       title: 'Consultório Mont’Serrat',
-      description:
-        <span>Av. Cel. Lucas De Oliveira, 505 - Sala 302 - Petrópolis, Porto Alegre - RS, <span className={styles.noWrap}>90440-011</span></span>
+      description: (
+        <span>
+          Av. Cel. Lucas De Oliveira, 505 - Sala 302 - Petrópolis, Porto Alegre
+          - RS, <span className={styles.noWrap}>90440-011</span>
+        </span>
+      )
     },
     {
       title: 'IGO Petrópolis',
-      description:
-        <span>Av. Sen. Tarso Dutra, 10 - 3º Andar - Petrópolis, Porto Alegre - RS, <span className={styles.noWrap}>90690-140</span></span>
+      description: (
+        <span>
+          Av. Sen. Tarso Dutra, 10 - 3º Andar - Petrópolis, Porto Alegre - RS,{' '}
+          <span className={styles.noWrap}>90690-140</span>
+        </span>
+      )
     },
     {
       title: 'IGO Canoas',
-      description: <span>R. Liberdade, 33 - Mal. Rondon, Canoas - RS, <span className={styles.noWrap}>92020-030</span></span>
+      description: (
+        <span>
+          R. Liberdade, 33 - Mal. Rondon, Canoas - RS,{' '}
+          <span className={styles.noWrap}>92020-030</span>
+        </span>
+      )
     },
     {
       title: 'Atendimento',
-      description: <span>De Segunda À Sexta Das 08:00 às 19h00. <span className={`${styles.noWrap} ${styles.bold}`}>(51) 99152-6125</span></span>
+      description: (
+        <span>
+          De Segunda À Sexta Das 08:00 às 19h00.{' '}
+          <span className={`${styles.noWrap} ${styles.bold}`}>
+            (51) 99152-6125
+          </span>
+        </span>
+      )
     }
   ];
 
