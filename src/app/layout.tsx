728f7558146e8c8661eaffb55ce849b72be84ece
Lucas Antunes <lucasba8@gmail.com>
2024-06-18 16:27:57 -0300

    feat: add google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import '@/globals.scss';
 
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import React from 'react';
 
 import { Footer } from '@/components/footer/footer.component';
@@ -21,6 +22,22 @@ export default function RootLayout({
   return (
     <html lang={'pt-br'}>
       <body>
+        <Script strategy={'beforeInteractive'} id={'google-tag'}>
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-KHLM7NS3');
+        `}
+        </Script>
+        <noscript>
+          <iframe
+            src={'https://www.googletagmanager.com/ns.html?id=GTM-KHLM7NS3'}
+            height={'0'}
+            width={'0'}
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <Navbar />
         <WhatsappButton />
         {children}
